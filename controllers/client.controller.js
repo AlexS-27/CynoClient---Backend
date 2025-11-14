1d6b554597d9907421e95323baf94be5b44ba00a
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-14 14:46:35 +0100

    feat: error management for Clients

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,19 +1,20 @@
 import { getAllClients, getClientById } from "../models/client.model.js";
 import IsValidId from "../utils/helper.mjs"
+import IsValidLimit from "../utils/helper.mjs"
 
 export const fetchAllClients = async (req, res, next) => {
     try {
         const limit = req.query.limit ? parseInt(req.query.limit, 10) : null;
+        if (limit !== null && (!isValidLimit(limit) || limit <= 0)) {
+            throw {status: 400, message: 'Limit must be a positive number.'};
+        }
         const clients = await getAllClients(limit);
-
+        if (!clients || !clients.length === 0) {
+            throw {status: 404, message: 'No clients found.'};
+        }
         res.status(200).json(clients);
     } catch (error) {
-        console.error("Error fetching clients:", error);
-        res.status(500).json({
-            status: 500,
-            error: "Internal Server Error",
-            message: "Failed to retrieve clients.",
-        });
+        next(error);
     }
 };
 
@@ -26,7 +27,7 @@ export const fetchClientById = async (req, res, next) => {
         const client = await getClientById(id);
 
         if (!client) {
-            throw {status: 400, message: "Client not found"};
+            throw {status: 404, message: "Client not found"};
         }
 
         res.status(200).json(client);
--- utils/helper.mjs
+++ utils/helper.mjs
@@ -1,3 +1,7 @@
 function isValidId(value) {
     return Number.isInteger(Number(value)) && Number(value) > 0;
 }
+
+function isValidLimit(value) {
+    return Number.isInteger(Number(value)) && Number(value) > 0;
+}
