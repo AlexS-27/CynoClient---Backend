00414cd9978cb435de3afdf3dccdd2fbc78e0a17
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-21 14:02:00 +0100

    fix: corrected error 503 management

--- models/chien.model.js
+++ models/chien.model.js
@@ -1,15 +1,9 @@
 import { db } from "../db/connection.js";
 
 export const getAllDogs = async (limit = null) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getAllDogs(limit);
 };
 
 export const getDogById = async (id) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getDogById(id);
 };
--- models/localite.model.js
+++ models/localite.model.js
@@ -1,15 +1,9 @@
 import { db } from "../db/connection.js";
 
 export const getAllLocations = async (limit = null) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getAllLocations(limit);
 };
 
 export const getLocationById = async (id) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getLocationById(id);
 };
