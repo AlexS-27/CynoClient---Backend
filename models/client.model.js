bd6fa429f219763bf8e92b61186fe5ca9738d1a4
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-21 13:59:02 +0100

    fix: corrected erro 503 management

--- models/client.model.js
+++ models/client.model.js
@@ -1,15 +1,9 @@
 import { db } from "../db/connection.js";
 
 export const getAllClients = async (limit = null) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getAllClients(limit);
 };
 
 export const getClientById = async (id) => {
-    if (!db.connectToDB()) {
-        throw { status: 503, message: "Database unavailable."}
-    }
     return await db.getClientById(id);
 };
