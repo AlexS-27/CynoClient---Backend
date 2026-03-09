fc24f22a132d245096f4adcafd1f327f95dbfe7f
Kilian Testard <pc47xiy@eduvaud.ch>
2026-03-09 09:43:52 +0100

    workflows: exercise 5

---
+++ workflow_ex5.js
@@ -0,0 +1,13 @@
+const age = 25
+const nom = "Alice"
+
+function direBonjour(prenom){
+    console.log("Bonjour " +prenom)
+    var message = "Bienvenue"
+    return message
+}
+
+direBonjour(nom)
+
+const resultat = 10+5
+console.log(resultat)
