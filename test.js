3ca1c0e8b9125b64e9e67a1ba04b702ebf53c240
AlexS-27 <akamanopro@gmail.com>
2026-03-09 10:24:09 +0100

    Feat: Add a new workflow

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-// test.js (pour JavaScript - avec des erreurs volontaires de style)
-function direBonjour( nom ){
-    console.log( "Bonjour "+nom )
+// test_sale.js - Le cauchemar des linters
+var x = 10; // Utilisation de 'var' (déconseillé en JS moderne)
+const y=20    // Mauvaise indentation + manque de point-virgule
+
+function  calcul(a,b){
+    if(a==b){ // Utilisation de '==' au lieu de '==='
+        return a+b; // Mauvaise indentation
+    }
+    console.log("Calcul terminé") // Console.log souvent interdit
 }
 
-direBonjour("ICT324")
+calcul(x , y);
